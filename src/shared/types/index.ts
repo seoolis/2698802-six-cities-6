4e3add4e23d1c1f3ec069492ec9230a7269b0e31
seoolis <anika777333@list.ru>
2026-03-25 17:35:14 +0500

    3.20. Добавлено перечисление Component

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -7,5 +7,6 @@ export * from './city.type.js';
 export * from './enums/amenities.type.enum.js';
 export * from './enums/house.type.enum.js';
 export * from './enums/user.type.enum.js';
+export * from './enums/component.enum';
 
 export * from './mock-server-data.type.js';
